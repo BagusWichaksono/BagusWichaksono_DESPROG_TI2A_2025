48a3aed2566f1fdde773f5edb33c91fd8b4ba19d
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2025-10-02 20:04:54 +0700

    p4

--- Pertemuan5/function.php
+++ Pertemuan5/function.php
@@ -1,14 +1,17 @@
 <?php
-
-function perkenalan() {
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Bagus<br/>";
+//membuat fungsi
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
-// Memanggil fungsi yang sudah dibuat dua kali
-perkenalan();
-echo "<hr>"; // Garis pemisah
-perkenalan();
+//memanggil fungsi yang sudah dibuat
+perkenalan("Hamdana", "Hallo");
 
+echo "<hr>";
+
+$saya = "Bagus";
+//memanggil lagi tanpa mengisi parameter salam
+perkenalan($saya);
 ?>
