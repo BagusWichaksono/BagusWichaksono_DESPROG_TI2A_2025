99e34f940d9fb97b7e30fe5d6bfe379a7bafbba7
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2025-10-02 20:16:35 +0700

    p4

--- Pertemuan5/function_return.php
+++ Pertemuan5/function_return.php
@@ -1,10 +1,20 @@
 <?php
-//membuat fungsi
+//membuat fungsi hitung umur
 function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
 
-echo "Umur saya adalah ". hitungUmur(2005, 2025) ." tahun"; 
-// isi sesuai dengan tahun lahir kalian
+//membuat fungsi perkenalan
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    
+    //memanggil fungsi lain
+    echo "Saya berusia ". hitungUmur(2005, 2025) ." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+//memanggil fungsi perkenalan
+perkenalan("Bagus");
 ?>
