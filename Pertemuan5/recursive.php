b642e9c7792ce3a1ec25d0b0df4e50dcb3a1c2a0
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2025-10-02 20:25:52 +0700

    p5

--- Pertemuan5/recursive.php
+++ Pertemuan5/recursive.php
@@ -1,11 +1,5 @@
 <?php
-function tampilkanAngka (int $jumlah, int $indeks = 1) {
-    echo "Perulangan ke-{$indeks} <br>";
-
-    //panggil diri sendiri selama $indeks < $jumlah
-    if ($indeks < $jumlah) {
-        tampilkanAngka($jumlah, $indeks + 1);
-    }
+for ($i=1; $i <= 25; $i++) {
+    echo "Perulangan ke-{$i} <br>";
 }
-tampilkanAngka(20);
 ?>
