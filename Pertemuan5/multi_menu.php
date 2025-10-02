cdecd3f4b166cdeb78a5ad45df34de35b4633256
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2025-10-02 20:33:15 +0700

    p6

--- Pertemuan5/multi_menu.php
+++ Pertemuan5/multi_menu.php
@@ -1,42 +1,28 @@
 <?php
 $menu = [
-    [
-        "nama" => "Beranda"
-    ],
-    [
-        "nama" => "Berita",
-        "subMenu" => [
-            [
-                "nama" => "Wisata",
-                "subMenu" => [
-                    [
-                        "nama" => "Pantai"
-                    ],
-                    [
-                        "nama" => "Gunung"
-                    ]
-                ]
-            ],
-            [
-                "nama" => "Kuliner"
-            ],
-            [
-                "nama" => "Hiburan"
-            ]
-        ]
-    ],
-    [
-        "nama" => "Tentang"
-    ],
-    [
-        "nama" => "Kontak"
-    ],
+    ["nama" => "Beranda"],
+    ["nama" => "Berita", "subMenu" => [
+        ["nama" => "Wisata", "subMenu" => [
+            ["nama" => "Pantai"],
+            ["nama" => "Gunung"]
+        ]],
+        ["nama" => "Kuliner"],
+        ["nama" => "Hiburan"]
+    ]],
+    ["nama" => "Tentang"],
+    ["nama" => "Kontak"],
 ];
 
 function tampilkanMenuBertingkat(array $menu) {
     echo "<ul>";
-    foreach ($menu as $key => $item) {
-        echo "<li>{$item['nama']}</li>";
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}";
+        // Periksa apakah ada subMenu di dalam item ini
+        if (isset($item['subMenu'])) {
+            // Jika ada, panggil fungsi ini lagi (rekursif) dengan subMenu sebagai argumen
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
+        echo "</li>";
     }
     echo "</ul>";
 }
