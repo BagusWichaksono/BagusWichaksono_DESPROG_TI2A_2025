585ac3e1688f8628b42c825f25541590c6f4a9a6
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2025-10-02 19:35:33 +0700

    p4

--- Pertemuan5/array_2.php
+++ Pertemuan5/array_2.php
@@ -3,19 +3,44 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Data Dosen</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        th, td {
+            border: 1px solid #ccc;
+            padding: 5px;
+        }
+    </style>
 </head>
 <body>
+    <h2>Data Dosen</h2>
     <?php
     $Dosen = [
         'nama' => 'Elok Nur Hamdana',
         'domisili' => 'Malang',
         'jenis_kelamin' => 'Perempuan'
     ];
-
-    echo "Nama: {$Dosen['nama']} <br>";
-    echo "Domisili: {$Dosen['domisili']} <br>";
-    echo "Jenis Kelamin: {$Dosen['jenis_kelamin']} <br>";
     ?>
+    
+    <table>
+        <tr>
+            <th>Keterangan</th>
+            <th>Data</th>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><?php echo $Dosen['nama']; ?></td>
+        </tr>
+        <tr>
+            <td>Domisili</td>
+            <td><?php echo $Dosen['domisili']; ?></td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+        </tr>
+    </table>
 </body>
 </html>
---
+++ Pertemuan5/function.php
@@ -0,0 +1,14 @@
+<?php
+
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Bagus<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+// Memanggil fungsi yang sudah dibuat dua kali
+perkenalan();
+echo "<hr>"; // Garis pemisah
+perkenalan();
+
+?>
