466e8ded5ae32f3955221ab1069262bb190e0f53
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2025-10-11 19:00:51 +0700

    p1soal

--- Pertemuan6/array_1.php
+++ Pertemuan6/array_1.php
@@ -3,13 +3,23 @@
 <head>
 </head>
 <body>
-<h2>Array Terindeks</h2>
+<h2>Array Terindeks dengan Perulangan</h2>
 <?php
     $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
 
-    echo $Listdosen[2] . "<br>";
-    echo $Listdosen[0] . "<br>";
-    echo $Listdosen[1] . "<br>";
+    // Menggunakan perulangan for
+    echo "Menggunakan for:<br>";
+    for ($i = 0; $i < count($Listdosen); $i++) {
+        echo $Listdosen[$i] . "<br>";
+    }
+
+    echo "<hr>";
+
+    // Menggunakan perulangan foreach
+    echo "Menggunakan foreach:<br>";
+    foreach ($Listdosen as $dosen) {
+        echo $dosen . "<br>";
+    }
 ?>
 </body>
 </html>
